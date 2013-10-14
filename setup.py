0ba3f97a719f681b51460114622b333c3edccc84
Eric Rich <erich@fenrir.redhat.com>
2013-10-14 15:57:01 -0400

    Example using code from http://www.cherrypy.org/

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='Example-CherryPy',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='OpenShift CherryPy Example',
+      author='Eric Rich',
+      author_email='sferich888@gamil.com',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      install_requires=['CherryPy>=3.2.4'],
      )
